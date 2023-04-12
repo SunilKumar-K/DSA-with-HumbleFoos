c87346a05c233b5cab57afe92e9c90683abfeda5
HumbleFool830 <sunilkatta830@gmail.com>
2023-04-12 18:03:55 +0530

    added multidimension array

--- Programming_Files/Arrays/arraysConcepts.java
+++ Programming_Files/Arrays/arraysConcepts.java
@@ -3,7 +3,30 @@ import java.util.Arrays;
 public class arraysConcepts{
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
+        singleDimension(arr);// calling a function 
 
+        int mul[][] = { {1,2,3},{4,5,6},{7,8,9}};
+        multiDimensionArray(mul);
+
+
+
+
+
+    }
+
+    public static void multiDimensionArray(int arr[][])
+    { for(int i=0;i<arr.length;i++)
+        {
+             for(int j=0;j<arr.length;j++)
+             {
+                System.out.print(arr[i][j]); //It will print the value with respect to the very index of i,j for given range.
+            }
+            System.out.println();//for new line
+        }
+}
+
+    public static void singleDimension(int arr[])
+    {
         // Accessing elements in an array .
 
         // First Method .
